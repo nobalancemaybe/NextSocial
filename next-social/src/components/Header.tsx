827d08f7b0cbade737058d6762c9adf8b13ace81
Stefan Trajkovski <121553629+nobalancemaybe@users.noreply.github.com>
2024-03-27 01:23:13 +0100

    dark mode toggle added

--- next-social/src/components/Header.tsx
+++ next-social/src/components/Header.tsx
@@ -1,13 +1,23 @@
+
+import DarkModeToggle from "./DarkModeToggle";
 import Logo from "./Logo";
 
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900"> 
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 
       bg-white dark:bg-gray-900 max-w-7xl mx-auto">
-        <Logo /> 
-           
+        <Logo />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          {/* {User &&
+          ...
+          } */}
+
+          <DarkModeToggle />
+
+          {/* {userbutton} */}
+        </div>
       </nav>
     </header>
   )
