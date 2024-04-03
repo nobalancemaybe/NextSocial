c93fa8798b351b99440b89c9a6b46e9a07b91a63
Stefan Trajkovski <121553629+nobalancemaybe@users.noreply.github.com>
2024-04-03 22:31:21 +0200

    registration form dialog

--- next-social/src/App.tsx
+++ next-social/src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header"
 import { Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import {UserContextProvider} from "../context/UserContext"
-import { RegistrationPage } from "./pages/RegistrationPage";
+
 
 // import { UserData } from "../context/AppContext";
 // import { auth } from "../firebase-config"
@@ -52,7 +52,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="registration-page" element={<RegistrationPage />}/>
         </Routes>
  
       </UserContextProvider>
---
+++ next-social/src/components/RegistrationForm.tsx.tsx
@@ -0,0 +1,84 @@
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Dialog } from "./ui/dialog"
+
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long"
+  }),
+})
+
+export const RegistrationForm = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit() {
+    console.log(form)
+  }
+
+  return (
+    <Dialog>
+
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Username" {...field} />
+                </FormControl>
+                {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your password" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </Dialog>
+  )
+}
