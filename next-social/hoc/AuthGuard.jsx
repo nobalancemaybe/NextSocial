b053d8bbee8b13955c85936d55e34c51d5f90e98
Stefan Trajkovski <121553629+nobalancemaybe@users.noreply.github.com>
2024-04-05 19:44:51 +0200

    added commented ts version revisit later

--- next-social/hoc/AuthGuard.jsx
+++ next-social/hoc/AuthGuard.jsx
@@ -1,11 +1,11 @@
-import { AuthContext } from "/context/AuthContext";
+import {UserContext} from '../context/UserContext.jsx'
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
 
 export default function AuthGuard({ children }) {
-  const { user } = useContext(AuthContext);
+  const { user } = useContext(UserContext);
   const location = useLocation();
 
   if (!user) {
@@ -18,3 +18,27 @@ export default function AuthGuard({ children }) {
 AuthGuard.propTypes = {
   children: PropTypes.node,
 };
+
+
+
+// TypeScript
+// import { UserContext } from "../context/UserContext"; // Assuming UserContext is a TypeScript file
+// import { useContext, ReactNode } from "react";
+// import { Navigate, useLocation } from "react-router-dom";
+
+
+// interface UserContextValue {
+//   user: User | null; // Define the type of user object
+// }
+
+// export default function AuthGuard({ children }: { children: ReactNode }) {
+//   const { user }: UserContextValue = useContext(UserContext);
+//   const location = useLocation();
+
+//   if (!user) {
+//     return <Navigate replace to="/register" state={{ from: location }} />;
+//   }
+
+//   return children;
+// }
+
--- next-social/src/components/RegistrationForm.tsx
+++ next-social/src/components/RegistrationForm.tsx
@@ -15,8 +15,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogDescription, DialogTitle, DialogFooter, DialogClose } from "./ui/dialog"
-// import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
 
 
 const FormSchema = z.object({
@@ -61,6 +60,7 @@ const FormSchema = z.object({
   gender: z.enum(["male", "female", "other"], {
     required_error: "Choose other if you don't want to specify",
   }),
+  createdOn: z.number()
 })
 
 export const RegistrationForm = () => {
@@ -68,21 +68,30 @@ export const RegistrationForm = () => {
     resolver: zodResolver(FormSchema),
     defaultValues: {
       username: "",
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      gender: undefined,
+      createdOn: Date.now(),
     },
   })
 
   function onSubmit() {
-    console.log(form)
+    console.log("submited")
   }
 
   return (
+
+
     <Dialog open>
       <DialogTrigger asChild>
         <Button variant="outline">
           Register
         </Button>
       </DialogTrigger>
-      <DialogContent className="flex flex-col items-center text-xl md:text-2xl lg:text-3xl w-full h-auto md:w-3/4 lg:w-1/2 md:h-auto lg:h-3/4 ">
+      <DialogContent className="flex flex-col items-center text-xl md:text-2xl lg:text-3xl w-full h-auto md:w-3/4 lg:w-1/2 md:h-auto lg:h-auto ">
+
         <DialogHeader className="flex items-center">
           <DialogTitle>Register</DialogTitle>
           <DialogDescription>
@@ -174,57 +183,60 @@ export const RegistrationForm = () => {
               name="confirmPassword"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Confirm Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter your password" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Type your password again
-                  </FormDescription>
-                  <FormMessage />
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                <Input placeholder="Enter your password" {...field} />
+                </FormControl>
+                <FormDescription>
+                Type your password again
+                </FormDescription>
+                <FormMessage />
                 </FormItem>
               )}
             /> */}
-            {/* <FormField
+            <FormField
               control={form.control}
               name="gender"
               render={({ field }) => (
                 <FormItem className="space-y-3">
                   <FormLabel>Choose your gender</FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                      className="flex flex-col space-y-1"
-                    >
-                      <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="male" />
-                        </FormControl>
-                        <FormLabel className="font-normal">
-                          Male
-                        </FormLabel>
-                      </FormItem>
-                      <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="female" />
-                        </FormControl>
-                        <FormLabel className="font-normal">
-                          Female
-                        </FormLabel>
-                      </FormItem>
-                      <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="other" />
-                        </FormControl>
-                        <FormLabel className="font-normal">Other</FormLabel>
-                      </FormItem>
-                    </RadioGroup>
-                  </FormControl>
+                  <div>
+
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex flex-col md:flex-row space-y-1 md:justify-between"
+                      >
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="male" />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            Male
+                          </FormLabel>
+                        </FormItem>
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="female" />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            Female
+                          </FormLabel>
+                        </FormItem>
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="other" />
+                          </FormControl>
+                          <FormLabel className="font-normal">Other</FormLabel>
+                        </FormItem>
+                      </RadioGroup>
+                    </FormControl>
+                  </div>
                   <FormMessage />
                 </FormItem>
               )}
-            /> */}
+            />
             <div className="flex flex-col items-center mt-6 mb-2 lg:mt-12 lg:mb-4">
               <Button type="submit" className="lg:px-8 lg:py-2 lg:text-xl lg:shadow-md">
                 Create your account
@@ -239,8 +251,10 @@ export const RegistrationForm = () => {
             </Button>
           </DialogClose>
         </DialogFooter>
+
       </DialogContent>
 
     </Dialog>
+
   )
 }
